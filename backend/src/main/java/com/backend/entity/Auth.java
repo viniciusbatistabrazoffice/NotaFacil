07dd6e20c612f1c6c723267dfbab4a8b7aaf313b
viniciusbatistadev <viniciusbatistabrazoffice@gmail.com>
2026-09-11 10:05:35 -0300

    Implementando a entidade auth

---
+++ backend/src/main/java/com/backend/entity/Auth.java
@@ -0,0 +1,53 @@
+package com.backend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity 
+@Table(name = "auth")
+public class Auth {
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+
+    public Auth(Long id, String username, String password, String email) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+}
